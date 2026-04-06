ea52dca792f9a626f827fecef3af6af458df9f09
Zhenya Markov <zhenya15.markov@mail.ru>
2026-04-06 16:20:36 +0300

    Based template FastAPI

---
+++ apps/fast_api/src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from core import config
+
+app = FastAPI(
+    # Конфигурируем название проекта. Оно будет отображаться в документации
+    title=config.PROJECT_NAME,
+    # Адрес документации в красивом интерфейсе
+    docs_url='/api/openapi',
+    # Адрес документации в формате OpenAPI
+    openapi_url='/api/openapi.json',
+    # Можно сразу сделать небольшую оптимизацию сервиса
+    # и заменить стандартный JSON-сериализатор на более шуструю версию, написанную на Rust
+    default_response_class=ORJSONResponse,
+)
